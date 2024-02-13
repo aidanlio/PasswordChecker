74550626168ebd7467f7e37c95184fd3a60b4b6d
aidanlio <aidanlio@outlook.com>
2024-02-13 13:47:15 -0500

    modified

--- password_safety_checker.py
+++ password_safety_checker.py
@@ -1,6 +1,6 @@
 from getpass import getpass
 
-special_characters = ["!", "\" "" #", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ""]", "^", "_", "`", "{", "|", "}", "~", ]
+special_characters = ["!", "\", "" #", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ""]", "^", "_", "`", "{", "|", "}", "~", ]
 
 print("This tool utilizes the annual Hive Systems Password Table \n This information provided should only be used as a guide and not as a means of ensuring security")
 password = getpass("Please enter your password? ")
